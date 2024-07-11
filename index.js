7f390695dfa1f3c75b342706a117dd1935e85612
Abel Koech <koechabel161@gmail.com>
2024-07-11 10:52:53 +0300

    fetch

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+ return fetch("https://anapioficeandfire.com/api/books")
+ .then(function(response){
+    response.json()
+    .then(function(data){
+      renderBooks(data)})
+  })
   
-}
-
+  }
+  
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
